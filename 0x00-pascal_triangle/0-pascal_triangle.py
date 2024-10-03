29b515b8d2fde6004475d0bdf06e6c8c5878a0b9
Jedidiah Abera <yididi@Jedidiah>
2024-10-04 00:36:45 +0300

    solved

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-"""
-Pascal's Triangle
-"""
+'''Solution of Pascal's Triangle'''
 
 
 def pascal_triangle(n):
-    """Creating a function def pascal_triangle(n): it returns a list of lists of integers that represent the Pascal’s triangle of n
-    """
-    result = []
-    if n > 0:
-        for i in range(1, n + 1):
-            level = []
-            C = 1
-            for j in range(1, i + 1):
-                level.append(C)
-                C = C * (i - j) // j
-            result.append(level)
-    return result
+    '''Pascal's Triangle Function'''
+    if n <= 0:
+        return []
+    pascal = [[1]]
+    for i in range(n - 1):
+        temp = [0] + pascal[-1] + [0]
+        pascal_row = []
+        for j in range(len(pascal[-1]) + 1):
+            pascal_row.append(temp[j] + temp[j+1])
+        pascal.append(pascal_row)
+    return pascal

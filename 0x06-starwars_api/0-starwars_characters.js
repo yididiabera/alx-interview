ad03d5980feedd69864e7af89cd330647e6533bf
Jedidiah Abera <140195943+yididiabera@users.noreply.github.com>
2024-11-14 22:31:13 +0300

    solved

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,43 +3,35 @@ const movieId = process.argv[2];
 const request = require('request');
 
 if (!movieId) {
-  console.error('Usage: ./starwars_characters.js <movie_id>');
-  process.exit(1);
+    console.error('Usage: ./0-starwars_characters.js <movie_id>');
+    process.exit(1);
 }
 
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
-
-  if (response.statusCode === 200) {
-    const characters = JSON.parse(body).characters;
-    
-    function fetchCharacter(characterUrl) {
-      return new Promise((resolve, reject) => {
-        request(characterUrl, (error, response, body) => {
-          if (error) {
-            reject(error);
-          }
-          if (response.statusCode === 200) {
-            resolve(JSON.parse(body).name);
-          } else {
-            reject(new Error('Failed to fetch character'));
-          }
-        });
-      });
+request(url, async function (error, response, body) {
+    if (error) {
+        console.error(error);
     }
-
-    Promise.all(characters.map(fetchCharacter))
-      .then((characterNames) => {
-        characterNames.forEach(name => console.log(name));
-      })
-      .catch((error) => console.error('Error fetching characters:', error));
-  } else {
-    console.error('Failed to fetch movie data');
-  }
+    if (response.statusCode === 200) {
+        const characters = JSON.parse(body).characters;
+        function fetchCharacter(character) {
+            return new Promise((resolve, reject) => {
+                request(character, (error, response, body) => {
+                    if (error) {
+                        reject(error);
+                    }
+                    if (response.statusCode === 200) {
+                        resolve(JSON.parse(body).name);
+                    }
+                    reject(new Error('Request failed'));
+                })
+            })
+        }
+        Promise.all(characters.map(fetchCharacter))
+            .then((characters) => {
+                characters.forEach((character) => console.log(character));
+            })
+            .catch((error) => console.error(error));
+    }
 });
-
